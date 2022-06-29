65d712888c126a1cada0eeba63a405b21283a0a1
LAPTOP-0HQOAFD7\Pranav <pranavkamat11@gmail.com>
2022-06-29 12:56:10 +0530

    improving chat bot

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,5 +1,6 @@
 from chatterbot import  ChatBot
 from chatterbot.trainers import ListTrainer
+from tkinter import *
 
 bot = ChatBot('My Bot')
 
@@ -24,13 +25,52 @@ trainer = ListTrainer(bot)
 
 trainer.train(convo)
 
-print("talk to Bot")
-while True:
-    query=input()
-    if query=='exit':
-        break
-    answer = bot.get_response(query)
-    print("bot : ",answer)
+# print("talk to Bot")
+# while True:
+#     query=input()
+#     if query=='exit':
+#         break
+#     answer = bot.get_response(query)
+#     print("bot : ",answer)
+#
+# print(answer)
 
-print(answer)
 
+# creating window
+
+
+
+
+main = Tk()
+
+main.geometry("500x650")
+main.title("My Chat Bot")
+img = PhotoImage(file="bot1.png")
+photoL = Label(main,image=img)
+
+photoL.pack(pady=5)
+
+def ask_from_bot() :
+    query = textF.get()
+    answer_from_bot = bot.get_response(query)
+    msgs.insert(END,"you : "+query)
+    msgs.insert(END,"bot : "+str(answer_from_bot))
+    textF.delete(0,END)
+
+
+frame = Frame(main)
+
+sc=Scrollbar(frame)
+msgs = Listbox(frame, width=80 , height=20)
+
+sc.pack(side=RIGHT, fill=Y)
+msgs.pack(side=LEFT, fill=BOTH , pady=10)
+frame.pack()
+
+# creating text field
+textF = Entry(main , font=("Verdana",20))
+textF.pack(fill=X , pady=10)
+
+btn=Button(main,text="Ask from Bot",font=("Verdana",20), command=ask_from_bot)
+btn.pack()
+main.mainloop()
